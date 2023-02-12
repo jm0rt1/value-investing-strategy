d4287ff7b0f5ecbeb08ba32d656e838a15adb9a6
Jamie Mortensen <16mortensenj@gmail.com>
2023-02-12 12:07:53 -0500

    model updates and outputed images

--- src/launcher/shared/alphavantage_helper.py
+++ src/launcher/shared/alphavantage_helper.py
@@ -2,6 +2,9 @@
 # UYOGYE4MI3DF16W2
 import requests
 from typing import Any
+
+import pandas as pd
+
 API_KEY = "UYOGYE4MI3DF16W2"
 
 
@@ -17,16 +20,45 @@ class AlphaVantageData():
 
 
 class AlphaVantageClient():
-    @staticmethod
-    def get_income_statement(symbol: str):
-        # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-        url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={API_KEY}'
-        r = requests.get(url)
-        data = r.json()
 
-        print(data)
+    class IncomeStatement():
+        @staticmethod
+        def print_json(symbol: str):
+            # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+            url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={API_KEY}'
+            r = requests.get(url)
+            data = r.json()
 
-    class FinancialOverview():
+            print(data)
+
+    class BalanceSheet():
+        @staticmethod
+        def print_json(symbol: str):
+            url = f'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={symbol}&apikey={API_KEY}'
+            r = requests.get(url)
+            data = r.json()
+
+            print(data)
+
+    class CashFlow():
+        @staticmethod
+        def print_json(symbol: str):
+            url = f'https://www.alphavantage.co/query?function=CASH_FLOW&symbol={symbol}&apikey={API_KEY}'
+            r = requests.get(url)
+            data = r.json()
+
+            print(data)
+
+    class Earnings():
+        @staticmethod
+        def print_json(symbol: str):
+            url = f'https://www.alphavantage.co/query?function=EARNINGS&symbol={symbol}&apikey={API_KEY}'
+            r = requests.get(url)
+            data = r.json()
+
+            print(data)
+
+    class CompanyOverview():
         @staticmethod
         def print_json(symbol: str):
             url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={API_KEY}'
@@ -36,8 +68,5 @@ class AlphaVantageClient():
 
             print(data)
 
-        @staticmethod
-        def
 
-
-AlphaVantageClient.FinancialOverview.print_json("AAPL")
+AVC = AlphaVantageClient
