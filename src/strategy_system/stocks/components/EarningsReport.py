97c9acf923adf15181459ba8d4d9a942d4a167e5
Jamie Mortensen <16mortensenj@gmail.com>
2023-03-01 17:43:28 -0500

    generated stock components

--- src/strategy_system/stocks/components/EarningsReport.py
+++ src/strategy_system/stocks/components/EarningsReport.py
@@ -1,12 +1,31 @@
 
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from stocks.components.StockComponent import StockComponent
 
-class EarningsReport(StockComponent):
-    def __init__(self):
-        self.fiscalDateEnding = None
-        self.reportedDate = None
-        self.reportedEPS = None
-        self.estimatedEPS = None
+from dataclasses import dataclass
 
+
+@dataclass
+class EarningsReport(StockComponent):
+    fiscal_date_ending: str
+    reported_date: str
+    reported_eps: float
+    estimated_eps: float
+    surprise_percentage: float
+    surprise_dollar: float
+    year_ago: float
+    year_ago_change_percentage: float
+
+    @classmethod
+    def from_dict(cls, data: dict[str, str]) -> 'EarningsReport':
+        fiscal_date_ending = data.get('fiscalDateEnding', '')
+        reported_date = data.get('reportedDate', '')
+        reported_eps = float(data.get('reportedEPS', 0))
+        estimated_eps = float(data.get('estimatedEPS', 0))
+        surprise_percentage = float(data.get('surprisePercentage', 0))
+        surprise_dollar = float(data.get('surpriseDollar', 0))
+        year_ago = float(data.get('yearAgo', 0))
+        year_ago_change_percentage = float(data.get('yearAgoChangePercent', 0))
+        company_id = data.get('symbol', '')
+        return cls(fiscal_date_ending, reported_date, reported_eps, estimated_eps, surprise_percentage, surprise_dollar, year_ago, year_ago_change_percentage, company_id)
--- src/strategy_system/stocks/components/IncomeStatement.py
+++ src/strategy_system/stocks/components/IncomeStatement.py
@@ -1,7 +1,44 @@
 
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-from stocks.components.StockComponent import StockComponent
+from dataclasses import dataclass
+from typing import List, Dict, Union
+from src.strategy_system.stocks.components.StockComponent import StockComponent
+from src.strategy_system.stocks.components.IncomeReport import IncomeReport
 
+
+@dataclass
 class IncomeStatement(StockComponent):
-    pass
+    symbol: str
+    annual_reports: List[IncomeReport]
+    quarterly_reports: List[IncomeReport]
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, Union[str, list[dict[str, str]]]]) -> 'IncomeStatement':
+        annual_reports: list[IncomeReport] = []
+        quarterly_reports: list[IncomeReport] = []
+
+        raw_annual_reports: list[dict[str, str]
+                                 ] = data.get("annualReports", [])
+
+        raw_qtrly_reports: list[dict[str, str]
+                                ] = data.get("quarterlyReports", [])
+
+        if type(raw_annual_reports) is str:
+            raise TypeError("Annual reports should not be a string")
+        if type(raw_qtrly_reports) is str:
+            raise TypeError("Quarterly reports should not be a string")
+
+        for report in raw_annual_reports:
+            annual_reports.append(IncomeReport.from_dict(report))
+        for report in raw_qtrly_reports:
+            quarterly_reports.append(IncomeReport.from_dict(report))
+
+        symbol = data.get("symbol", "")
+        if type(symbol) is not str:
+            raise TypeError("symbol needs to be a string")
+        return cls(
+            symbol=symbol,
+            annual_reports=annual_reports,
+            quarterly_reports=quarterly_reports
+        )
