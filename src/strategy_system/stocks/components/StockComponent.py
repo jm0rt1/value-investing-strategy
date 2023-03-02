024fdd0853307c615f98264d177f5a64007cf058
Jamie Mortensen <16mortensenj@gmail.com>
2023-03-01 22:00:46 -0500

    added a from_json_file method

--- src/strategy_system/stocks/components/StockComponent.py
+++ src/strategy_system/stocks/components/StockComponent.py
@@ -1,10 +1,17 @@
 
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-class StockComponent:
+from pathlib import Path
+
+from abc import ABC
+
+
+class StockComponent(ABC):
     def __init__(self):
         pass
 
     def from_alpha_vantage_data(self, cls, data):
         pass
 
+    def from_json_file(self, path: Path):
+        pass
