c23b55b45ad922a82338a315af5277f0e5bfa5c2
Jamie Mortensen <16mortensenj@gmail.com>
2023-03-05 13:04:47 -0500

    strategy system updated with inteface methods

--- src/strategy_system/StrategySystem.py
+++ src/strategy_system/StrategySystem.py
@@ -18,3 +18,60 @@ class StrategySystem(StrategyInterface):
 
     def get_RebalancingStrategy(self, uid):
         pass
+
+    def run(self, ):
+        pass
+
+    def initialize(self, ):
+        pass
+
+    def reset(self, ):
+        pass
+
+    def calculate_book_value_dividend_projections_valuation(self, ticker: str):
+        pass
+
+    def compare_qfa(self, tickers):
+        pass
+
+    def save_qfa(self, analysis_to_save):
+        pass
+
+    def calculate_graham_number(self, ticker):
+        pass
+
+    def calculate_discounted_cash_flow_valuation(self, ticker):
+        pass
+
+    def calculate_asset_based_valuation(self, ticker):
+        pass
+
+    def calculator_financial_metric_analysis(self, ticker):
+        pass
+
+    def calculate_financial_metric_analysis_valuation(self, ticker):
+        pass
+
+    def generate_qf_analysis(self, ):
+        pass
+
+    def generate_rebalancing_analysis(self, ):
+        pass
+
+    def add_rebalancing_dataflow(self, analyzer):
+        pass
+
+    def add_qfa_dataflow(self, analyzer):
+        pass
+
+    def run_rebalance(self, uid):
+        pass
+
+    def run_qfa(self, uid):
+        pass
+
+    def get_settings(self, ):
+        pass
+
+    def compare_stocks(self, tickers):
+        pass
