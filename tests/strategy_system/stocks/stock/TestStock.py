d0297a71b1de3f2df986cce0dc75e938215282ab
Jamie Mortensen <16mortensenj@gmail.com>
2023-03-18 15:36:23 -0400

    unittest for stock added

---
+++ tests/strategy_system/stocks/stock/TestStock.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+
+import unittest
+
+from src.strategy_system.stocks.stock.Stock import Stock
+from pathlib import Path
+
+from src.strategy_system.stocks.stock.components.BalanceSheet import BalanceReport
+from src.strategy_system.stocks.stock.components.IncomeStatement import IncomeStatement, IncomeReport
+from src.strategy_system.stocks.stock.components.Earnings import EarningsReport, EarningsStatement
+
+TEST_PATH = Path(
+    "tests/test_files/inputs/strategy_system/stocks/stock/components")
+
+
+class TestStock(unittest.TestCase):
+    def test_from_alpha_vantage_data(self):
+        stock = Stock.from_alpha_vantage_data('AAPL', TEST_PATH)
+
+        # balance sheet
+        self.assertEqual(len(stock.balance_sheet.annual_reports), 5)
+        self.assertIsInstance(
+            stock.balance_sheet.annual_reports[0], BalanceReport)
+        self.assertEqual(len(stock.balance_sheet.quarterly_reports), 20)
+        self.assertIsInstance(
+            stock.balance_sheet.quarterly_reports[0], BalanceReport)
+
+        self.assertIsInstance(
+            stock.income_statement, IncomeStatement)
+        self.assertEqual(len(stock.income_statement.annual_reports), 5)
+        self.assertIsInstance(
+            stock.income_statement.annual_reports[0], IncomeReport)
+        self.assertEqual(len(stock.income_statement.quarterly_reports), 20)
+        self.assertIsInstance(
+            stock.income_statement.quarterly_reports[0], IncomeReport)
+
+        self.assertIsInstance(stock.earnings, EarningsStatement)
+        self.assertEqual(len(stock.earnings.annual_reports), 28)
+        self.assertIsInstance(
+            stock.earnings.annual_reports[0], EarningsReport)
+        self.assertEqual(len(stock.earnings.quarterly_reports), 108)
+        self.assertIsInstance(
+            stock.earnings.quarterly_reports[0], EarningsReport)
