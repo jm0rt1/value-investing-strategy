32f7c9165934da25187207ded5d6059d022fdb6a
Jamie Mortensen <16mortensenj@gmail.com>
2023-03-18 09:57:36 -0400

    copied a dict in to the test file

--- tests/strategy_system/stocks/components/TestEarnings.py
+++ tests/strategy_system/stocks/components/TestEarnings.py
@@ -1,1 +1,26 @@
 # TODO
+
+from pathlib import Path
+
+
+TEST_PATH = Path(
+    "tests/test_files/inputs/strategy_system/stocks/stock/components/AAPL.Earnings.json")
+DATA = {
+    "symbol": "AAPL",
+    "annualEarnings": [
+        {
+            "fiscalDateEnding": "2022-12-31",
+            "reportedEPS": "1.88"
+        }
+    ],
+    "quarterlyEarnings": [
+        {
+            "fiscalDateEnding": "2022-12-31",
+            "reportedDate": "2023-02-02",
+            "reportedEPS": "1.88",
+            "estimatedEPS": "1.94",
+            "surprise": "-0.06",
+            "surprisePercentage": "-3.0928"
+        }
+    ]
+}
