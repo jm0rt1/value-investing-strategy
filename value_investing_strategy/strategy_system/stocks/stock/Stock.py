2381675411a8a472d14832adc4e970f51ae8a907
Varis Nijat <vnijat93@gmail.com>
2023-03-20 20:57:38 -0400

    DEV Update data file path

--- value_investing_strategy/strategy_system/stocks/stock/Stock.py
+++ value_investing_strategy/strategy_system/stocks/stock/Stock.py
@@ -1,4 +1,4 @@
-
+import os
 
 from dataclasses import dataclass
 import math
@@ -10,8 +10,10 @@ from value_investing_strategy.strategy_system.stocks.stock.components.CashFlow i
 from value_investing_strategy.strategy_system.stocks.stock.components.CompanyOverview import CompanyOverview
 
 
-PATH_TO_STOCK_DATA = Path(
-    "./value_investing_strategy/data/SimpleAlphaVantageCacher/output/json_cache/DATA")
+module_path = os.path.dirname(__file__)
+data_file_path = os.path.join(module_path, 'data/SimpleAlphaVantageCacher/output/json_cache/DATA')
+
+PATH_TO_STOCK_DATA = Path(data_file_path)
 
 
 @dataclass
